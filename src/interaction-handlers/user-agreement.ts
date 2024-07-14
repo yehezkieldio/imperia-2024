69c1d54d51dc957078444ab282f89ff58fff9b84
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-14 18:39:43 +0800

    feat: add user agreement interaction handler

---
+++ src/interaction-handlers/user-agreement.ts
@@ -0,0 +1,74 @@
+import { USER_AGREEMENT_ID } from "@/internal/constants/ids";
+import { db } from "@/internal/database/connection";
+import { users } from "@/internal/database/schema";
+import { ImperiaEmbedBuilder } from "@/internal/extensions/embed-builder";
+import { UserAgreementStatus } from "@/internal/types/enums";
+import { InteractionHandler, InteractionHandlerTypes } from "@sapphire/framework";
+import type { ButtonInteraction } from "discord.js";
+
+interface ParsedData {
+    userId: string;
+    status: UserAgreementStatus;
+}
+
+export class UserAgreementHandler extends InteractionHandler {
+    public constructor(context: InteractionHandler.LoaderContext, options: InteractionHandler.Options) {
+        super(context, {
+            ...options,
+            name: "user-agreement-handler",
+            interactionHandlerType: InteractionHandlerTypes.Button,
+        });
+    }
+
+    public parse(interaction: ButtonInteraction) {
+        if (interaction.customId.startsWith(USER_AGREEMENT_ID)) {
+            const [_, userId, status] = interaction.customId.split("-");
+            const currentStatus = status as UserAgreementStatus;
+
+            if (currentStatus === UserAgreementStatus.CONFIRMED) {
+                return this.some<ParsedData>({ userId, status: UserAgreementStatus.CONFIRMED });
+            }
+
+            if (currentStatus === UserAgreementStatus.CANCELLED) {
+                return this.some<ParsedData>({ userId, status: UserAgreementStatus.CANCELLED });
+            }
+
+            return this.none();
+        }
+
+        return this.none();
+    }
+
+    public async run(interaction: ButtonInteraction, data?: ParsedData) {
+        if (!data) return;
+
+        await interaction.update({
+            components: [],
+        });
+
+        if (data.status === UserAgreementStatus.CONFIRMED) {
+            return interaction.reply({
+                embeds: [
+                    new ImperiaEmbedBuilder().setDescription(
+                        "You have declined Imperia's user agreement, and have not been registered. Some features may not be available to you, should you change your mind, you can re-run the register command.",
+                    ),
+                ],
+                ephemeral: true,
+            });
+        }
+
+        await db.transaction(async (tx) => {
+            await tx.insert(users).values({
+                discordId: data.userId,
+            });
+        });
+
+        return interaction.editReply({
+            embeds: [
+                new ImperiaEmbedBuilder().setDescription(
+                    "You have agreed to Imperia's user agreement and have been registered. Welcome and enjoy your stay!",
+                ),
+            ],
+        });
+    }
+}
