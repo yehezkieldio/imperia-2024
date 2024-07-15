679fdcfe96f29f38729d341adae7ae58f9744430
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-16 06:27:29 +0800

    feat: add a command to show all available commands

--- src/internal/constants/ids.ts
+++ src/internal/constants/ids.ts
@@ -1,3 +1,5 @@
 export const USER_AGREEMENT_ID = "user_agreement" as const;
 export const RESPONSE_PRIVACY_ID = "response_privacy" as const;
 export const HELP_COMMAND_ID = "help_command_select_menu" as const;
+export const COMMANDS_LIST_PREVIOUS_PAGE_ID = "commands_list_previous_page" as const;
+export const COMMANDS_LIST_NEXT_PAGE_ID = "commands_list_next_page" as const;
--- src/internal/utils/command-utils.ts
+++ src/internal/utils/command-utils.ts
@@ -1,6 +1,34 @@
 import { container } from "@sapphire/framework";
 import { chatInputApplicationCommandMention } from "discord.js";
 
+// based on the folder names of the commands
+export const commandCategories = [
+    {
+        name: "Configuration",
+        value: "configuration",
+    },
+    {
+        name: "Entertainment",
+        value: "entertainment",
+    },
+    {
+        name: "Information",
+        value: "information",
+    },
+    {
+        name: "Miscellaneous",
+        value: "miscellaneous",
+    },
+    {
+        name: "Moderation",
+        value: "moderation",
+    },
+    {
+        name: "System",
+        value: "system",
+    },
+];
+
 export const getCommandMention = (commandName: string): string | `</${string}:${string}>` => {
     const command = container.applicationCommandRegistries.acquire(commandName);
     const commandId = command.globalChatInputCommandIds.values().next().value;
