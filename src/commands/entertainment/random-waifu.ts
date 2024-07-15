f9f148db9cef16846224a90833f8deed4909cb89
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-15 17:14:29 +0800

    feat: random waifu command

---
+++ src/commands/entertainment/random-waifu.ts
@@ -0,0 +1,38 @@
+import { ImperiaCommand } from "@/internal/extensions/command";
+import { ImperiaEmbedBuilder } from "@/internal/extensions/embed-builder";
+import { RegisterBehavior } from "@sapphire/framework";
+import { SlashCommandBuilder } from "discord.js";
+
+export class RandomWaifuCommand extends ImperiaCommand {
+    public constructor(context: ImperiaCommand.Context, options: ImperiaCommand.Options) {
+        super(context, {
+            description: "Retrive a random waifu image from the internet",
+            requiredClientPermissions: ["SendMessages"],
+            ...options,
+        });
+    }
+
+    public override registerApplicationCommands(registry: ImperiaCommand.Registry) {
+        const command = new SlashCommandBuilder().setName(this.name).setDescription(this.description);
+
+        void registry.registerChatInputCommand(command, {
+            behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
+            guildIds: [],
+            idHints: [],
+        });
+    }
+
+    public async chatInputRun(interaction: ImperiaCommand.ChatInputCommandInteraction) {
+        return interaction.reply({
+            embeds: [
+                new ImperiaEmbedBuilder().setDescription(`~Here's your waifu! :3`).setImage(await this.getWaifu()),
+            ],
+        });
+    }
+
+    public async getWaifu() {
+        const response = await fetch("https://api.waifu.pics/sfw/waifu");
+        const data = await response.json();
+        return data.url;
+    }
+}
