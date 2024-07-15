a1bbe1259dadb1bd1fbc02ac2b2a6cd52e3b380f
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-15 19:56:55 +0800

    feat: add last command usage listing command

---
+++ src/commands/system/cmd-usage.ts
@@ -0,0 +1,112 @@
+import { DEVELOPMENT_SERVERS } from "@/internal/constants/developers";
+import { db } from "@/internal/database/connection";
+import { commandUsage } from "@/internal/database/schema";
+import { ImperiaCommand } from "@/internal/extensions/command";
+import { RegisterBehavior } from "@sapphire/framework";
+import dayjs from "dayjs";
+import { SlashCommandBuilder } from "discord.js";
+import { eq } from "drizzle-orm";
+
+export class LastCommandUsageCommand extends ImperiaCommand {
+    public constructor(context: ImperiaCommand.Context, options: ImperiaCommand.Options) {
+        super(context, {
+            description: "Display a list of the last cmds used in the server, or a user for the last five minutes.",
+            requiredClientPermissions: ["SendMessages"],
+            preconditions: ["GuildOnly"],
+            ...options,
+        });
+    }
+
+    public override registerApplicationCommands(registry: ImperiaCommand.Registry) {
+        const command = new SlashCommandBuilder()
+            .setName(this.name)
+            .setDescription(this.description)
+            .addUserOption((option) =>
+                option.setName("user").setDescription("The user to display the last commands for.").setRequired(false),
+            );
+
+        void registry.registerChatInputCommand(command, {
+            behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
+            guildIds: DEVELOPMENT_SERVERS,
+            idHints: [],
+        });
+    }
+
+    public async chatInputRun(interaction: ImperiaCommand.ChatInputCommandInteraction) {
+        const user = interaction.options.getUser("user");
+
+        if (!user) {
+            const lastCommands = await db.select().from(commandUsage);
+
+            const filteredCommands = lastCommands.filter((entry) => {
+                const entryTime = dayjs(entry.timestamp);
+                const currentTime = dayjs();
+                const difference = currentTime.diff(entryTime, "minute");
+
+                return difference <= 5;
+            });
+
+            if (filteredCommands.length === 0) {
+                return interaction.reply({
+                    content: "No commands have been used in the last five minutes.",
+                    ephemeral: await ImperiaCommand.isEphemeralResponse(interaction.user.id),
+                });
+            }
+
+            filteredCommands.splice(20);
+
+            const mapCommands = filteredCommands.map(async (entry) => {
+                return `Server ${await this.mapGuildIdToName(entry.guildId)} - ${entry.command} - ${dayjs(entry.timestamp).format("HH:mm:ss")}`;
+            });
+
+            const commands = await Promise.all(mapCommands);
+
+            return interaction.reply({
+                content: commands.join("\n"),
+                ephemeral: await ImperiaCommand.isEphemeralResponse(interaction.user.id),
+            });
+        }
+
+        const lastCommands = await db.select().from(commandUsage).where(eq(commandUsage.discordId, user.id));
+
+        const filteredCommands = lastCommands.filter((entry) => {
+            const entryTime = dayjs(entry.timestamp);
+            const currentTime = dayjs();
+            const difference = currentTime.diff(entryTime, "minute");
+
+            return difference <= 5;
+        });
+
+        if (filteredCommands.length === 0) {
+            return interaction.reply({
+                content: `No commands have been used by ${user.username} in the last five minutes.`,
+                ephemeral: await ImperiaCommand.isEphemeralResponse(interaction.user.id),
+            });
+        }
+
+        filteredCommands.splice(20);
+
+        const mapCommands = filteredCommands.map(async (entry) => {
+            return `Server: ${await this.mapGuildIdToName(entry.guildId)} - User: ${await this.mapUserIdToUsername(entry.discordId)} - ${entry.command} - ${dayjs(entry.timestamp).format("HH:mm:ss")}`;
+        });
+
+        const commands = await Promise.all(mapCommands);
+
+        return interaction.reply({
+            content: commands.join("\n"),
+            ephemeral: await ImperiaCommand.isEphemeralResponse(interaction.user.id),
+        });
+    }
+
+    private async mapUserIdToUsername(userId: string) {
+        const user = await this.container.client.users.fetch(userId);
+
+        return user.username;
+    }
+
+    private async mapGuildIdToName(guildId: string) {
+        const guild = await this.container.client.guilds.fetch(guildId);
+
+        return guild.name;
+    }
+}
--- src/internal/database/schema.ts
+++ src/internal/database/schema.ts
@@ -1,5 +1,5 @@
 import { cuid2 } from "drizzle-cuid2/postgres";
-import { boolean, pgEnum, pgTable, pgTableCreator, timestamp, uniqueIndex, varchar } from "drizzle-orm/pg-core";
+import { boolean, index, pgEnum, pgTable, pgTableCreator, timestamp, uniqueIndex, varchar } from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM.
@@ -26,14 +26,25 @@ export const users = pgTable(
 
 export const commandUsageStatus = pgEnum("command_usage_status", ["success", "denied", "error"]);
 
-export const commandUsage = pgTable("command_usage", {
-    id: cuid2("id").defaultRandom().primaryKey(),
-    /**
-     * The Discord user ID of the user who used the command.
-     */
-    discordId: varchar("userId").notNull(),
-    guildId: varchar("guildId").notNull(),
-    command: varchar("command").notNull(),
-    timestamp: timestamp("timestamp").notNull(),
-    status: commandUsageStatus("status").notNull(),
-});
+export const commandUsage = pgTable(
+    "command_usage",
+    {
+        id: cuid2("id").defaultRandom().primaryKey(),
+        /**
+         * The Discord user ID of the user who used the command.
+         */
+        discordId: varchar("userId").notNull(),
+        guildId: varchar("guildId").notNull(),
+        command: varchar("command").notNull(),
+        timestamp: timestamp("timestamp").notNull(),
+        status: commandUsageStatus("status").notNull(),
+    },
+    (table) => {
+        return {
+            discordIdIdx: index("command_usage_discord_id_idx").on(table.discordId),
+            guildIdIdx: index("command_usage_guild_id_idx").on(table.guildId),
+            commandIdx: index("command_usage_command_idx").on(table.command),
+            timestampIdx: index("command_usage_timestamp_idx").on(table.timestamp),
+        };
+    },
+);
