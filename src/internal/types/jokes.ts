f266d444dcd9ab05feec07c1db006db09400f796
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-16 05:14:03 +0800

    feat: add random jokes command

---
+++ src/internal/types/jokes.ts
@@ -0,0 +1,21 @@
+export interface RandomJokesQueryResult {
+    error: boolean;
+    category: "Programming" | "Misc" | "Dark" | "Pun" | "Spooky" | "Christmas";
+    type: "twopart" | "single";
+    joke: string;
+    setup: string;
+    delivery: string;
+    flags: Flags;
+    id: number;
+    safe: boolean;
+    lang: string;
+}
+
+interface Flags {
+    nsfw: boolean;
+    religious: boolean;
+    political: boolean;
+    racist: boolean;
+    sexist: boolean;
+    explicit: boolean;
+}
