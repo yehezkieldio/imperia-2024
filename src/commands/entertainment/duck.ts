552346778725bcf809762d475e5bbf0112636fa7
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-19 06:34:13 +0800

    feat: add a random duck image command

---
+++ src/commands/entertainment/duck.ts
@@ -0,0 +1,82 @@
+import { ImperiaCommand } from "@/internal/extensions/command";
+import { ImperiaEmbedBuilder } from "@/internal/extensions/embed-builder";
+import { ImperiaIdentifiers } from "@/internal/extensions/identifiers";
+import { UserError } from "@sapphire/framework";
+import { type Message, SlashCommandBuilder } from "discord.js";
+
+interface RandomDuckResponse {
+    message?: string;
+    url: string;
+}
+
+export class DuckCommand extends ImperiaCommand {
+    public constructor(context: ImperiaCommand.Context, options: ImperiaCommand.Options) {
+        super(context, {
+            description: "Get a random duck image.",
+            requiredClientPermissions: ["SendMessages"],
+            preconditions: ["GuildOnly"],
+            ...options,
+        });
+    }
+
+    public override registerApplicationCommands(registry: ImperiaCommand.Registry) {
+        const command = new SlashCommandBuilder().setName(this.name).setDescription(this.description);
+
+        void registry.registerChatInputCommand(command, {
+            guildIds: [],
+            idHints: [],
+        });
+    }
+
+    public async chatInputRun(interaction: ImperiaCommand.ChatInputCommandInteraction): Promise<Message> {
+        await interaction.deferReply({
+            ephemeral: await this.utils.responsePrivacy(interaction.user.id),
+        });
+
+        const result: RandomDuckResponse | undefined = await this.getRandomCat();
+
+        if (!result) {
+            return interaction.editReply({
+                embeds: [
+                    new ImperiaEmbedBuilder()
+                        .isErrorEmbed()
+                        .setDescription("No duck images were found, please try again later."),
+                ],
+            });
+        }
+
+        return interaction.editReply({
+            embeds: [new ImperiaEmbedBuilder().isSuccessEmbed().setImage(result.url)],
+        });
+    }
+
+    private async getRandomCat(): Promise<RandomDuckResponse | undefined> {
+        const cacheKey = "random_ducks";
+        const url = "https://random-d.uk/api/random";
+
+        try {
+            const response = (await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "User-Agent": "Imperia (https://github.com/i9ntheory/imperia)",
+                    Accept: "application/json",
+                },
+            }).then((response: Response) => response.json())) as RandomDuckResponse;
+
+            await this.container.df.sadd(cacheKey, response.url);
+
+            return response;
+        } catch (error) {
+            this.container.logger.error(error);
+
+            const cached: string | null = await this.container.df.srandmember(cacheKey);
+            if (cached) return { url: cached };
+
+            new UserError({
+                identifier: ImperiaIdentifiers.CommandServiceError,
+                message: "An error occurred while fetching the duck image, please try again later.",
+            });
+        }
+    }
+}
