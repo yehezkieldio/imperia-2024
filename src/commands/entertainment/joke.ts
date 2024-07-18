aa6f7d13b99368c1d9a27c4d29b5ef926b821fc0
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-19 05:33:59 +0800

    feat: add a random joke command

---
+++ src/commands/entertainment/joke.ts
@@ -0,0 +1,101 @@
+import { ImperiaCommand } from "@/internal/extensions/command";
+import { ImperiaEmbedBuilder } from "@/internal/extensions/embed-builder";
+import { ImperiaIdentifiers } from "@/internal/extensions/identifiers";
+import { UserError } from "@sapphire/framework";
+import { type Message, SlashCommandBuilder } from "discord.js";
+
+interface RandomJokeResponse {
+    error?: boolean;
+    category?: "Programming" | "Misc" | "Dark" | "Pun" | "Spooky" | "Christmas";
+    type?: "twopart" | "single";
+    joke: string;
+    setup?: string;
+    delivery?: string;
+    flags?: Flags;
+    id?: number;
+    safe?: boolean;
+    lang?: string;
+}
+
+interface Flags {
+    nsfw: boolean;
+    religious: boolean;
+    political: boolean;
+    racist: boolean;
+    sexist: boolean;
+    explicit: boolean;
+}
+
+export class JokeCommand extends ImperiaCommand {
+    public constructor(context: ImperiaCommand.Context, options: ImperiaCommand.Options) {
+        super(context, {
+            description: "Get a random joke.",
+            requiredClientPermissions: ["SendMessages"],
+            preconditions: ["GuildOnly"],
+            ...options,
+        });
+    }
+
+    public override registerApplicationCommands(registry: ImperiaCommand.Registry) {
+        const command = new SlashCommandBuilder().setName(this.name).setDescription(this.description);
+
+        void registry.registerChatInputCommand(command, {
+            guildIds: [],
+            idHints: [],
+        });
+    }
+
+    public async chatInputRun(interaction: ImperiaCommand.ChatInputCommandInteraction): Promise<Message> {
+        await interaction.deferReply({
+            ephemeral: await this.utils.responsePrivacy(interaction.user.id),
+        });
+
+        const result: RandomJokeResponse | undefined = await this.getRandomJoke();
+
+        if (!result) {
+            return interaction.editReply({
+                embeds: [
+                    new ImperiaEmbedBuilder()
+                        .isErrorEmbed()
+                        .setDescription("No joke were found, please try again later."),
+                ],
+            });
+        }
+
+        return interaction.editReply({
+            embeds: [new ImperiaEmbedBuilder().isSuccessEmbed().setDescription(result.joke)],
+        });
+    }
+
+    private async getRandomJoke(): Promise<RandomJokeResponse | undefined> {
+        const cacheKey = "random_jokes";
+        const url = "https://sv443.net/jokeapi/v2/joke/Any";
+
+        try {
+            const response = (await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "User-Agent": "Imperia (https://github.com/i9ntheory/imperia)",
+                    Accept: "application/json",
+                },
+            }).then((response: Response) => response.json())) as RandomJokeResponse;
+
+            if (response.type === "twopart") response.joke = `${response.setup}\n${response.delivery}`;
+
+            await this.container.df.sadd(cacheKey, response.joke);
+
+            return response;
+        } catch (error) {
+            this.container.logger.error(error);
+
+            const cached: string | null = await this.container.df.srandmember(cacheKey);
+            if (cached) return { joke: cached };
+
+            new UserError({
+                identifier: ImperiaIdentifiers.CommandServiceError,
+                message: "An error occurred while fetching the cat fact, please try again later.",
+            });
+        }
+    }
+}
