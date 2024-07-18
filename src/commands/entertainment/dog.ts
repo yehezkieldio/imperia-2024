5f252bd6eea110aa2e92d110eea889e30a278740
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-18 14:17:25 +0800

    feat: add random dog and cat images

---
+++ src/commands/entertainment/dog.ts
@@ -0,0 +1,74 @@
+import { ImperiaCommand } from "@/internal/extensions/command";
+import { ImperiaEmbedBuilder } from "@/internal/extensions/embed-builder";
+import { ImperiaIdentifiers } from "@/internal/types/identifiers";
+import type { RandomDogQueryResult } from "@/internal/types/images";
+import {} from "@/internal/utils/string-utils";
+import { RegisterBehavior, UserError } from "@sapphire/framework";
+import { SlashCommandBuilder } from "discord.js";
+
+export class DogCommand extends ImperiaCommand {
+    public constructor(context: ImperiaCommand.Context, options: ImperiaCommand.Options) {
+        super(context, {
+            description: "Get a random dog image.",
+            requiredClientPermissions: ["SendMessages"],
+            preconditions: ["GuildOnly"],
+            ...options,
+        });
+    }
+
+    public override registerApplicationCommands(registry: ImperiaCommand.Registry) {
+        const command = new SlashCommandBuilder().setName(this.name).setDescription(this.description);
+
+        void registry.registerChatInputCommand(command, {
+            behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
+            guildIds: [],
+            idHints: [],
+        });
+    }
+
+    public async chatInputRun(interaction: ImperiaCommand.ChatInputCommandInteraction) {
+        await interaction.deferReply({
+            ephemeral: await ImperiaCommand.isEphemeralResponse(interaction.user.id),
+        });
+
+        const result = await this.getRandomDog();
+
+        if (!result) {
+            return interaction.editReply({
+                embeds: [new ImperiaEmbedBuilder().setDescription("No results found for the provided search.")],
+            });
+        }
+
+        const response = new ImperiaEmbedBuilder();
+
+        response.setImage(result.message);
+
+        return interaction.editReply({
+            embeds: [response],
+        });
+    }
+
+    private async getRandomDog(): Promise<RandomDogQueryResult | undefined> {
+        const url = "https://dog.ceo/api/breeds/image/random";
+
+        try {
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "User-Agent": "Imperia (https://github.com/i9ntheory/imperia)",
+                    Accept: "application/json",
+                },
+            }).then((response) => response.json());
+
+            return response;
+        } catch (error) {
+            this.container.logger.error(error);
+
+            new UserError({
+                identifier: ImperiaIdentifiers.SearchResultsNotFound,
+                message: "No results found for the provided search.",
+            });
+        }
+    }
+}
---
+++ src/internal/types/cat.ts
@@ -0,0 +1,8 @@
+export interface RandomCatQueryResult {
+    tags: string[];
+    mimetype: string;
+    size: number;
+    createdAt: string;
+    editedAt: string;
+    _id: string;
+}
