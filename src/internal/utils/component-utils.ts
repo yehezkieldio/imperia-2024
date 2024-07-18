12fb82e2241deee5fc8e9374372878c11615b558
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-19 07:13:19 +0800

    feat: add a tic tac toe game command

---
+++ src/internal/utils/component-utils.ts
@@ -0,0 +1,50 @@
+import {
+    ActionRowBuilder,
+    type ButtonBuilder,
+    type MessageActionRowComponentBuilder,
+    type MessageComponentInteraction,
+} from "discord.js";
+
+export function updateComponent<T extends ButtonBuilder>(
+    interaction: MessageComponentInteraction,
+    newButtonFunc: (component: T) => T,
+    customId = interaction.customId,
+): ActionRowBuilder<MessageActionRowComponentBuilder>[] {
+    const indices = findComponent(interaction, customId);
+    if (!indices) {
+        return [];
+    }
+
+    const actionRows = interaction.message.components.map<ActionRowBuilder<MessageActionRowComponentBuilder>>((row) =>
+        ActionRowBuilder.from(row),
+    );
+    newButtonFunc(actionRows[indices.actionRowIndex].components[indices.componentIndex] as T);
+
+    return actionRows;
+}
+
+export function disableComponent(
+    interaction: MessageComponentInteraction,
+    customId = interaction.customId,
+): ActionRowBuilder<MessageActionRowComponentBuilder>[] {
+    return updateComponent(interaction, (button) => button.setDisabled(true), customId);
+}
+
+export function findComponent(
+    interaction: MessageComponentInteraction,
+    customId: string,
+): { actionRowIndex: number; componentIndex: number } | undefined {
+    const actionRows = interaction.message.components;
+    for (let actionRowIndex = 0; actionRowIndex < actionRows.length; ++actionRowIndex) {
+        const actionRow = actionRows[actionRowIndex];
+
+        for (let componentIndex = 0; componentIndex < actionRow.components.length; ++componentIndex) {
+            if (actionRow.components[componentIndex].customId === customId) {
+                return {
+                    actionRowIndex,
+                    componentIndex,
+                };
+            }
+        }
+    }
+}
