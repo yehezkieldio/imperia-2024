af88223260e38823e724c7e6dd9133be59f95ffb
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-19 18:35:38 +0800

    feat: add about command

--- src/internal/utils/command-utils.ts
+++ src/internal/utils/command-utils.ts
@@ -1,8 +1,36 @@
 import { users } from "@/internal/database/postgres/schema";
 import { type Container, container as c } from "@sapphire/framework";
+import { chatInputApplicationCommandMention } from "discord.js";
 
 export class CommandUtils {
     private container: Container = c;
+
+    public commandCategories = [
+        {
+            name: "Configuration",
+            value: "configuration",
+        },
+        {
+            name: "Entertainment",
+            value: "entertainment",
+        },
+        {
+            name: "Information",
+            value: "information",
+        },
+        {
+            name: "Miscellaneous",
+            value: "miscellaneous",
+        },
+        {
+            name: "Moderation",
+            value: "moderation",
+        },
+        {
+            name: "System",
+            value: "system",
+        },
+    ];
 
     public async responsePrivacy(userId: string): Promise<boolean> {
         const [user] = await this.container.db.select().from(users);
@@ -24,4 +52,13 @@ export class CommandUtils {
     public randomizeArray<T>(array: T[]): T {
         return array[Math.floor(Math.random() * array.length)];
     }
+
+    public getCommandMention = (commandName: string): string | `</${string}:${string}>` => {
+        const command = this.container.applicationCommandRegistries.acquire(commandName);
+        const commandId = command.globalChatInputCommandIds.values().next().value;
+
+        if (!commandId) return `/${commandName}`;
+
+        return chatInputApplicationCommandMention(command.commandName, commandId);
+    };
 }
