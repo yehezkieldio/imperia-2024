45f55c44043f0086eb5dc2d75cbd16b0bdf3b28d
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-21 10:33:31 +0800

    refactor: check if emojis exists on the data store

--- src/core/database/dragonfly/emoji/load-emoji.ts
+++ src/core/database/dragonfly/emoji/load-emoji.ts
@@ -2,6 +2,10 @@ import { container } from "@sapphire/framework";
 import emojiData from "./emoji-data.json";
 
 export async function loadEmoji(): Promise<void> {
+    if (await container.df.exists("emojis")) {
+        return;
+    }
+
     await container.df.call("JSON.SET", "emojis", "$", JSON.stringify(emojiData));
 }
 
