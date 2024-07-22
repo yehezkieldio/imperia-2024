8ee140d24896cd494dbc3deff70eb76dd0af17f3
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-22 12:58:39 +0800

    feat: add register command

---
+++ src/commands/miscellaneous/register.ts
@@ -0,0 +1,89 @@
+import { ImperiaCommand } from "@/core/extensions/command";
+import { ImperiaEmbedBuilder } from "@/core/extensions/embed-builder";
+import { CommandOptionsRunTypeEnum } from "@sapphire/framework";
+import { Time } from "@sapphire/time-utilities";
+import { TimerManager } from "@sapphire/timer-manager";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, type Message, SlashCommandBuilder } from "discord.js";
+
+export enum UserAgreementStatus {
+    CONFIRMED = "confirmed",
+    DECLINED = "declined",
+}
+
+export class RegisterCommand extends ImperiaCommand {
+    public constructor(context: ImperiaCommand.Context, options: ImperiaCommand.Options) {
+        super(context, {
+            description: "Create a new account on Imperia.",
+            runIn: CommandOptionsRunTypeEnum.GuildText,
+            ...options,
+        });
+    }
+
+    public override registerApplicationCommands(registry: ImperiaCommand.Registry): void {
+        const command = new SlashCommandBuilder().setName(this.name).setDescription(this.description);
+
+        void registry.registerChatInputCommand(command, {
+            guildIds: [],
+            idHints: [],
+        });
+    }
+
+    public async chatInputRun(interaction: ImperiaCommand.ChatInputCommandInteraction): Promise<void> {
+        const { embed, components } = this.generateAgreement(interaction.user.id);
+
+        interaction.reply({
+            embeds: [embed],
+            components: [components],
+            ephemeral: true,
+        });
+
+        TimerManager.setTimeout(() => {
+            interaction.editReply({
+                components: [],
+            });
+        }, Time.Second * 60);
+    }
+
+    public async messageRun(message: Message): Promise<void> {
+        const registerCommand: string = this.container.utilities.toolbox.getCommandMention("register");
+
+        const msg: Message = await message.reply(`Please use ${registerCommand} slash command to create an account.`);
+
+        TimerManager.setTimeout(() => {
+            if (msg.deletable) msg.delete();
+        }, Time.Second * 60);
+    }
+
+    private generateAgreement(userId: string) {
+        const embed: ImperiaEmbedBuilder = new ImperiaEmbedBuilder();
+
+        embed
+            .setTitle("Welcome to Imperia!")
+            .setDescription(
+                "Imperia will be collecting data from you to create your account within the system, please review the agreement below, click the agree button to proceed.",
+            );
+
+        embed.setFields([
+            {
+                name: "— Data Collection and Usage",
+                value: "Imperia gathers various Discord-related identifiers and stores content provided by users to deliver its functionalities. This information is solely used to enable the intended features of Imperia and is not utilized for any other purposes.",
+            },
+        ]);
+
+        const components = new ActionRowBuilder<ButtonBuilder>().addComponents(
+            new ButtonBuilder()
+                .setCustomId(`register-${userId}-${UserAgreementStatus.CONFIRMED}`)
+                .setLabel("Agree")
+                .setStyle(ButtonStyle.Success),
+            new ButtonBuilder()
+                .setCustomId(`register-${userId}-${UserAgreementStatus.DECLINED}`)
+                .setLabel("Decline")
+                .setStyle(ButtonStyle.Danger),
+        );
+
+        return {
+            embed,
+            components,
+        };
+    }
+}
