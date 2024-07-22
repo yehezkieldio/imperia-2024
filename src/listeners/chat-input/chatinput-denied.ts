198a462f3aac6e5915d177da9176181756df58a4
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-22 09:14:00 +0800

    refactor: move the command denied response embed into the toolbox

--- src/listeners/chat-input/chatinput-denied.ts
+++ src/listeners/chat-input/chatinput-denied.ts
@@ -1,9 +1,7 @@
-import { ImperiaEmbedBuilder } from "@/core/extensions/embed-builder";
+import type { ImperiaEmbedBuilder } from "@/core/extensions/embed-builder";
 import { ImperiaListener } from "@/core/extensions/listener";
 import { ImperiaEvents } from "@/core/types/events";
-import { ImperiaIdentifiers } from "@/core/types/identifiers";
 import type { ChatInputCommandDeniedPayload, UserError } from "@sapphire/framework";
-import { capitalizeFirstLetter } from "@sapphire/utilities";
 import type { InteractionResponse, Message } from "discord.js";
 
 export class ChatInputCommandDeniedListener extends ImperiaListener {
@@ -32,42 +30,7 @@ export class ChatInputCommandDeniedListener extends ImperiaListener {
             `ChatInputCommandErrorListener: Denied execution of slash command ${payload.command.name} by ${payload.interaction.user.id} in ${payload.interaction.guildId}.`,
         );
 
-        const { toolbox } = this.container.utilities;
-        const embed: ImperiaEmbedBuilder = new ImperiaEmbedBuilder().isWarningEmbed();
-
-        embed.setFooter({
-            text: `Error Identifier: ${capitalizeFirstLetter(error.identifier)}`,
-        });
-
-        switch (error.identifier) {
-            // General command errors
-            case ImperiaIdentifiers.CommandDisabled:
-                embed.setDescription("This command is currently disabled!");
-                break;
-            case ImperiaIdentifiers.PreconditionCooldown:
-                embed.setTitle("This command is on cooldown!");
-                embed.setDescription("Please wait for the cooldown to expire.");
-                break;
-            case ImperiaIdentifiers.PreconditionRunIn:
-                embed.setTitle("This command is not available in this context!");
-                embed.setDescription(`Please use this command in a ${toolbox.getChannelType(error)}`);
-                break;
-
-            // Permission errors
-            case ImperiaIdentifiers.PreconditionClientPermissions ||
-                ImperiaIdentifiers.PreconditionClientPermissionsNoPermissions:
-                embed.setTitle("I am missing required permissions to execute this command!");
-                embed.setDescription(`Required permission(s): ${toolbox.getMissingPermissions(error)}`);
-                break;
-            case ImperiaIdentifiers.PreconditionUserPermissions ||
-                ImperiaIdentifiers.PreconditionUserPermissionsNoPermissions:
-                embed.setTitle("You are missing required permissions to execute this command!");
-                embed.setDescription(`Required permission(s): ${toolbox.getMissingPermissions(error)}`);
-                break;
-            default:
-                embed.setDescription("Unhandled error occurred while executing this command!");
-                break;
-        }
+        const embed: ImperiaEmbedBuilder = this.container.utilities.toolbox.generateErrorEmbed(error);
 
         if (payload.interaction.deferred || payload.interaction.replied) {
             return payload.interaction.editReply({
--- src/utilities/toolbox.ts
+++ src/utilities/toolbox.ts
@@ -1,5 +1,8 @@
+import { ImperiaEmbedBuilder } from "@/core/extensions/embed-builder";
+import { ImperiaIdentifiers } from "@/core/types/identifiers";
 import type { UserError } from "@sapphire/framework";
 import { Utility } from "@sapphire/plugin-utilities-store";
+import { capitalizeFirstLetter } from "@sapphire/utilities";
 import { ChannelType, inlineCode } from "discord.js";
 
 export class Toolbox extends Utility {
@@ -8,6 +11,46 @@ export class Toolbox extends Utility {
             ...options,
             name: "toolbox",
         });
+    }
+
+    public generateErrorEmbed(error: UserError) {
+        const embed: ImperiaEmbedBuilder = new ImperiaEmbedBuilder().isWarningEmbed();
+
+        embed.setFooter({
+            text: `Error Identifier: ${capitalizeFirstLetter(error.identifier)}`,
+        });
+
+        switch (error.identifier) {
+            // General command errors
+            case ImperiaIdentifiers.CommandDisabled:
+                embed.setDescription("This command is currently disabled!");
+                break;
+            case ImperiaIdentifiers.PreconditionCooldown:
+                embed.setTitle("This command is on cooldown!");
+                embed.setDescription("Please wait for the cooldown to expire.");
+                break;
+            case ImperiaIdentifiers.PreconditionRunIn:
+                embed.setTitle("This command is not available in this context!");
+                embed.setDescription(`Please use this command in a ${this.getChannelType(error)}`);
+                break;
+
+            // Permission errors
+            case ImperiaIdentifiers.PreconditionClientPermissions ||
+                ImperiaIdentifiers.PreconditionClientPermissionsNoPermissions:
+                embed.setTitle("I am missing required permissions to execute this command!");
+                embed.setDescription(`Required permission(s): ${this.getMissingPermissions(error)}`);
+                break;
+            case ImperiaIdentifiers.PreconditionUserPermissions ||
+                ImperiaIdentifiers.PreconditionUserPermissionsNoPermissions:
+                embed.setTitle("You are missing required permissions to execute this command!");
+                embed.setDescription(`Required permission(s): ${this.getMissingPermissions(error)}`);
+                break;
+            default:
+                embed.setDescription("Unhandled error occurred while executing this command!");
+                break;
+        }
+
+        return embed;
     }
 
     public getChannelType(error: UserError) {
