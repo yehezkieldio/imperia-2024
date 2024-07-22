407fbc005446ac2005a28d3caa5942f11e51dbf8
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-22 12:29:46 +0800

    refactor: remove user response privacy from schema

--- src/core/database/postgres/schema.ts
+++ src/core/database/postgres/schema.ts
@@ -1,14 +1,5 @@
 import { cuid2 } from "drizzle-cuid2/postgres";
-import {
-    type PgTableFn,
-    boolean,
-    pgEnum,
-    pgTable,
-    pgTableCreator,
-    timestamp,
-    uniqueIndex,
-    varchar,
-} from "drizzle-orm/pg-core";
+import { type PgTableFn, pgEnum, pgTable, pgTableCreator, timestamp, uniqueIndex, varchar } from "drizzle-orm/pg-core";
 
 export const createTable: PgTableFn = pgTableCreator((name) => `imperia_${name}`);
 
@@ -17,10 +8,6 @@ export const users = pgTable(
     {
         id: cuid2("id").defaultRandom().primaryKey(),
         discordId: varchar("discordId").notNull(),
-        /**
-         * The response privacy for slash commands, either true for ephemeral or false for normal.
-         */
-        responsePrivacy: boolean("responsePrivacy").default(false),
     },
     (user) => ({
         discordIdUidx: uniqueIndex("user_discord_id_uidx").on(user.discordId),
