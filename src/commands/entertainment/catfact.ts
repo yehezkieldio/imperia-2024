68cb19f0a9101ed0516b41a185a2b737cd0b9b0e
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-30 05:45:53 +0800

    feat: add random cat fact command

---
+++ src/commands/entertainment/catfact.ts
@@ -0,0 +1,65 @@
+import { ImperiaCommand } from "@/lib/extensions/command";
+import { ImperiaEmbedBuilder } from "@/lib/extensions/embed-builder";
+import { ImperiaIdentifiers } from "@/lib/extensions/identifiers";
+import { FetchResultTypes, fetch } from "@sapphire/fetch";
+import { CommandOptionsRunTypeEnum, UserError } from "@sapphire/framework";
+import { type InteractionResponse, type Message, SlashCommandBuilder } from "discord.js";
+
+interface RandomCatFact {
+    fact: string;
+}
+
+export class CatFactCommand extends ImperiaCommand {
+    public constructor(context: ImperiaCommand.Context, options: ImperiaCommand.Options) {
+        super(context, {
+            ...options,
+            description: "Get a random cat fact.",
+            aliases: ["catfacts"],
+            tags: ["text", "fun"],
+            runIn: CommandOptionsRunTypeEnum.GuildText,
+        });
+    }
+
+    public override registerApplicationCommands(registry: ImperiaCommand.Registry): void {
+        const command = new SlashCommandBuilder().setName(this.name).setDescription(this.description);
+
+        void registry.registerChatInputCommand(command);
+    }
+
+    public async chatInputRun(interaction: ImperiaCommand.ChatInputCommandInteraction): Promise<InteractionResponse> {
+        return interaction.reply("This command is under construction.");
+    }
+
+    public async messageRun(message: Message): Promise<Message> {
+        const result = await this.getFact();
+
+        if (!result) {
+            throw new UserError({
+                identifier: ImperiaIdentifiers.CommandServiceError,
+                message: "( ノ・・)ノ I can't seem to fetch a cat fact right now, please try again later.",
+            });
+        }
+
+        return message.reply({ content: result.reply, embeds: [result.embed] });
+    }
+
+    private async getFact() {
+        const url: string = "https://catfact.ninja/fact";
+
+        try {
+            const response = await fetch<RandomCatFact>(url, FetchResultTypes.JSON);
+
+            const reply = "˖ ݁𖥔 ݁˖ Here's a random cat fact for you~";
+
+            const embed: ImperiaEmbedBuilder = new ImperiaEmbedBuilder().setColorScheme("info");
+            embed.setDescription(response.fact);
+
+            return {
+                reply: reply,
+                embed: embed,
+            };
+        } catch (error) {
+            this.container.logger.error(error);
+        }
+    }
+}
