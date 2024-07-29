561f74c92bd2fd8f1cd08580b3c686b1ef94e979
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-29 16:52:54 +0800

    fix: add fetch retry incase of timeouts

--- src/services/reddit.ts
+++ src/services/reddit.ts
@@ -1,6 +1,6 @@
 import { Service } from "@/lib/stores/services";
 import type { PullpushSubmission } from "@/lib/types/pullpush";
-import { FetchResultTypes, fetch } from "@sapphire/fetch";
+import { FetchResultTypes, QueryError, fetch } from "@sapphire/fetch";
 import { pickRandom, sleep } from "@sapphire/utilities";
 
 interface RedditSubmission {
@@ -30,14 +30,14 @@ export class RedditService extends Service {
     }
 
     public async _postLoad(): Promise<void> {
-        // We wait for 2 seconds to ensure other components are loaded, before we start setting up the cache.
-        await sleep(2000);
+        // We wait for 3 seconds to ensure other components are loaded, before we start setting up the cache.
+        await sleep(3000);
 
-        this.container.logger.info("RedditService: Setting up caches for subreddits");
+        this.container.logger.info("RedditService: Setting up caches for subreddits, this may take a while...");
         for (const subreddit of subreddits) {
             await this.setupCache(subreddit);
         }
-        this.container.logger.info("RedditService: Subreddit caches setup complete");
+        this.container.logger.info("RedditService: Subreddit caches setup complete and ready for use.");
     }
 
     public async rebuildCache(subreddit: AvailableSubreddit): Promise<void> {
@@ -68,7 +68,7 @@ export class RedditService extends Service {
         const type: string = pickRandom(sortTypes);
 
         const url: string = `https://api.pullpush.io/reddit/submission/search?html_decode=True&subreddit=${subreddit}&size=100&sort_type=${type}`;
-        const submissions: PullpushSubmission = await fetch(url, FetchResultTypes.JSON);
+        const submissions: PullpushSubmission = await this.fetchRetry(url, 3);
 
         const data: RedditSubmission[] = submissions.data
             .filter((data): string => data.url)
@@ -83,8 +83,23 @@ export class RedditService extends Service {
             submission.url.includes("i.redd.it"),
         );
 
-        await this.container.db.dragonfly.call("JSON.SET", `cache:${cacheKey}`, "$", JSON.stringify(filteredData));
+        await this.container.db.dragonfly.call("JSON.SET", `${cacheKey}`, "$", JSON.stringify(filteredData));
         await this.container.db.dragonfly.expire(cacheKey, 7600);
+    }
+
+    private async fetchRetry(url: string, retries: number): Promise<PullpushSubmission> {
+        try {
+            return await fetch<PullpushSubmission>(url, FetchResultTypes.JSON);
+        } catch (error) {
+            if (error instanceof QueryError) {
+                if (retries === 0) throw error;
+                await sleep(3000);
+                this.container.logger.error("RedditService: Fetch failed, retrying...");
+                return this.fetchRetry(url, retries - 1);
+            }
+
+            throw error;
+        }
     }
 
     public async getRandom(subreddit: AvailableSubreddit): Promise<RedditSubmission> {
