8f7bb8570048d9101586efe160acffd2f772b0e1
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-07-29 12:34:17 +0800

    feat: a script to flush dragonfly cache

--- scripts/dragonfly-flush.ts
+++ scripts/dragonfly-flush.ts
@@ -1,9 +1,19 @@
 import { dragonflyClient } from "@/lib/databases/dragonfly/client";
 
+const flags: string[] = Bun.argv.filter((arg) => arg.startsWith("--"));
+
 async function flushAllData(): Promise<void> {
+    const flushAll: boolean = flags.includes("--all");
+
     try {
-        await dragonflyClient.flushdb();
-        console.log("Successfully flushed all data from the dragonfly data store");
+        if (flushAll) {
+            await dragonflyClient.flushall();
+            console.log("Successfully flushed all data from all databases in the dragonfly data store.");
+        } else {
+            await dragonflyClient.flushdb();
+            console.log("Successfully flushed all data in the primary database in the dragonfly data store.");
+        }
+
         process.exit(0);
     } catch (error) {
         console.error("An error occurred while trying to flush all data from the database", error);
