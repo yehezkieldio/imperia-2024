41f4579391fb2829b9b73030afdf55a984eb2c43
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-08 03:25:23 +0800

    feat: add bot config

---
+++ src/config.ts
@@ -0,0 +1,52 @@
+import type { ImperiaClientOptions } from "@/lib/extensions/client";
+import { LogLevel } from "@sapphire/framework";
+import { Time } from "@sapphire/time-utilities";
+import { ActivityType, GatewayIntentBits, Partials } from "discord.js";
+
+export const DEVELOPERS: string[] = ["327849142774923266"];
+
+/**
+ * The configuration for the bot.
+ */
+export const configuration: ImperiaClientOptions = {
+    defaultPrefix: "imperia ",
+    allowedMentions: {
+        parse: [],
+        users: [],
+        roles: [],
+        repliedUser: true,
+    },
+    defaultCooldown: {
+        delay: Time.Second * 2,
+        filteredUsers: DEVELOPERS,
+    },
+    intents: [
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+    ],
+    loadApplicationCommandRegistriesStatusListeners: Bun.env.NODE_ENV === "production",
+    loadDefaultErrorListeners: Bun.env.NODE_ENV === "production",
+    loadSubcommandErrorListeners: true,
+    loadMessageCommandListeners: true,
+    logger: {
+        level: Bun.env.NODE_ENV === "production" ? LogLevel.Debug : LogLevel.Info,
+    },
+    overrideApplicationCommandsRegistries: Bun.env.NODE_ENV === "production",
+    partials: [Partials.Message, Partials.User, Partials.GuildMember],
+    presence: {
+        activities: [
+            {
+                type: ActivityType.Listening,
+                name: "reality, the manifested. ✨",
+            },
+        ],
+        status: "dnd",
+    },
+    subcommandDefaultCooldown: {
+        delay: Time.Second * 2,
+        filteredUsers: DEVELOPERS,
+    },
+    typing: true,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
-function main(): void {
-    console.log("Hello World!");
+import { envVariables } from "@/lib/env";
+
+// Parse the environment variables and verify their existence and correctness.
+envVariables.parse(process.env);
+
+import { configuration } from "@/config";
+import { ImperiaClient } from "@/lib/extensions/client";
+
+// Load plugin(s) and register them.
+import "@sapphire/plugin-logger/register";
+
+/**
+ * The main entrypoint for the bot.
+ */
+export async function main(): Promise<void> {
+    const client = new ImperiaClient(configuration);
+    await client.login(Bun.env.DISCORD_TOKEN);
+
+    process.on("SIGINT", async (): Promise<void> => {
+        await client.destroy().then((): never => {
+            process.exit();
+        });
+    });
 }
 
-main();
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
