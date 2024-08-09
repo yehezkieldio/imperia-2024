03dcaa3eccd7844f2fb1e22035643d9f62d281e2
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-09 19:44:41 +0800

    chore: add drizzle config file

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { envVariables } from "@/lib/env";
+
+// Parse the environment variables and verify their existence and correctness.
+envVariables.parse(process.env);
+
+import type { Config } from "drizzle-kit";
+
+export default {
+    schema: "./src/lib/databases/postgres/schema.ts",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: Bun.env.DATABASE_URL,
+    },
+    tablesFilter: ["imperia_*"],
+    out: "./migrations",
+} satisfies Config;
