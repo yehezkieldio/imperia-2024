2b007ce94655bec044915e0c91eac8e27a544313
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-09 22:40:02 +0800

    initial: initial rewrite

--- src/lib/types/union.ts
+++
@@ -1,5 +0,0 @@
-import type { ImperiaCommand } from "@/lib/extensions/commands/command";
-import type { InteractionResponse, Message } from "discord.js";
-
-export type MessageChatResponse = Message | InteractionResponse;
-export type MessageChatContext = Message | ImperiaCommand.ChatInputCommandInteraction;
