86338c0643b21eae530ec4e564142d55499db370
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-09 18:33:04 +0800

    feat: database schema

--- src/lib/databases/postgres/schema.ts
+++ src/lib/databases/postgres/schema.ts
@@ -1,5 +1,6 @@
 import {
     type PgTableFn,
+    index,
     pgEnum,
     pgTable,
     pgTableCreator,
@@ -11,43 +12,57 @@ import {
 
 export const createTable: PgTableFn = pgTableCreator((name) => `imperia_${name}`);
 
+/* -------------------------------------------------------------------------- */
+
 export const users = pgTable(
     "user",
     {
         id: uuid("id").defaultRandom().primaryKey(),
-        discordId: varchar("discordId").notNull(),
+        discordId: varchar("discord_id").notNull(),
     },
     (user) => ({
         discordIdUidx: uniqueIndex("user_discord_id_uidx").on(user.discordId),
     }),
 );
 
-export const commandStatus = pgEnum("command_history_status", ["success", "denied", "error", "unknown"]);
-export const commandType = pgEnum("command_history_type", ["slash", "message"]);
+/* -------------------------------------------------------------------------- */
 
-export const commandHistory = pgTable("command_history", {
-    id: uuid("id").defaultRandom().primaryKey(),
-    userId: varchar("userId").notNull(),
-    guildId: varchar("guildId").notNull(),
-    commandName: varchar("commandName").notNull(),
-    status: commandStatus("status").notNull(),
-    type: commandType("type").notNull(),
-    executedAt: timestamp("executedAt").defaultNow(),
-});
+const blacklistTypeEnum = pgEnum("blacklist_type", ["user", "guild"]);
+export type BlacklistType = (typeof blacklistTypeEnum.enumValues)[number];
 
-export const blacklistType = pgEnum("blacklist_entity_type", ["user", "guild"]);
-
-export const blacklistEntity = pgTable(
-    "blacklist_entity",
+export const blacklist = pgTable(
+    "blacklist",
     {
         id: uuid("id").defaultRandom().primaryKey(),
-        entityId: varchar("entityId").notNull(),
-        entityType: blacklistType("entityType").notNull(),
+        blacklistId: varchar("blacklist_id").notNull(),
+        blacklistType: blacklistTypeEnum("blacklist_type").notNull(),
     },
-    (blacklistEntity) => ({
-        entityIdTypeUidx: uniqueIndex("blacklist_entity_entity_id_type_uidx").on(
-            blacklistEntity.entityId,
-            blacklistEntity.entityType,
-        ),
+    (blacklist) => ({
+        blacklistIdUidx: uniqueIndex("blacklist_blacklist_id_uidx").on(blacklist.blacklistId),
+        blacklistTypeEnumIdx: index("blacklist_blacklist_type_enum_idx").on(blacklist.blacklistType),
+    }),
+);
+
+/* -------------------------------------------------------------------------- */
+
+const commandResultTypeEnum = pgEnum("command_result_type", ["success", "error", "denied"]);
+export type CommandResultType = (typeof commandResultTypeEnum.enumValues)[number];
+
+export const analytics = pgTable(
+    "analytics",
+    {
+        id: uuid("id").defaultRandom().primaryKey(),
+        userId: varchar("user_id").notNull(),
+        guildId: varchar("guild_id").notNull(),
+        command: varchar("command").notNull(),
+        result: commandResultTypeEnum("result").notNull(),
+        createdAt: timestamp("created_at").defaultNow(),
+    },
+    (analytics) => ({
+        userIdIdx: index("analytics_user_id_idx").on(analytics.userId),
+        guildIdIdx: index("analytics_guild_id_idx").on(analytics.guildId),
+        commandIdx: index("analytics_command_idx").on(analytics.command),
+        resultIdx: index("analytics_result_idx").on(analytics.result),
+        createdAtIdx: index("analytics_created_at_idx").on(analytics.createdAt),
     }),
 );
