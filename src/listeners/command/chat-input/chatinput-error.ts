e339e49be9f85105c51505dd016137e7cf8524fb
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-09 19:18:11 +0800

    feat: chat input error listener

--- src/listeners/command/chat-input/chatinput-error.ts
+++ src/listeners/command/chat-input/chatinput-error.ts
@@ -1,19 +1,18 @@
 import { commandAnalytics } from "@/lib/databases/postgres/schema";
 import { ImperiaEvents } from "@/lib/extensions/constants/events";
 import type { MessageChatResponse } from "@/lib/types/union";
-import { type ChatInputCommandDeniedPayload, Listener, type UserError } from "@sapphire/framework";
-import type {} from "discord.js";
+import { type ChatInputCommandErrorPayload, Listener, type UserError } from "@sapphire/framework";
 
-export class ChatInputCommandDeniedListener extends Listener {
+export class ChatInputCommandErrorListener extends Listener {
     public constructor(context: Listener.LoaderContext, options: Listener.Options) {
         super(context, {
             ...options,
             once: false,
-            event: ImperiaEvents.ChatInputCommandDenied,
+            event: ImperiaEvents.ChatInputCommandError,
         });
     }
 
-    public async run(error: UserError, payload: ChatInputCommandDeniedPayload): Promise<MessageChatResponse> {
+    public async run(error: UserError, payload: ChatInputCommandErrorPayload): Promise<MessageChatResponse> {
         const { database, logger } = this.container;
         const { interaction, command } = payload;
 
@@ -23,20 +22,20 @@ export class ChatInputCommandDeniedListener extends Listener {
                 userId: interaction.user.id,
                 guildId: interaction.guildId as string,
                 command: command.name,
-                result: "denied",
+                result: "error",
                 type: "chatinput",
             })
             .returning();
 
         if (!entry) {
-            logger.warn("ChatInputCommandDeniedListener: Failed to insert a new analytics entry. Skipping...");
+            logger.warn("ChatInputCommandErrorListener: Failed to insert a new analytics entry. Skipping...");
         } else {
             logger.info(
-                `ChatInputCommandDeniedListener: Command "${command.name}" by user "${interaction.user.id}" in guild "${interaction.guildId}" was denied.`,
+                `ChatInputCommandErrorListener: Command "${command.name}" by user "${interaction.user.id}" in guild "${interaction.guildId}" errored.`,
             );
         }
 
-        logger.debug(`ChatInputCommandDeniedListener: ${error.identifier}`);
+        logger.debug(`ChatInputCommandErrorListener: ${error.identifier}`);
 
         return interaction.reply({
             content: `${error.identifier}\n${error.message}`,
